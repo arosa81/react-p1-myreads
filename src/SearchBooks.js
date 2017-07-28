8b9e5678a87fe6d3c00f292e95060e5e6da97482
Alexander Rosa <alexanderrosa@alexanders-mbp.lan>
2017-07-27 18:00:27 -0600

    feat: Added link in search page back button to go back to home page

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class  SearchBooks extends Component {
   render() {
@@ -6,7 +7,7 @@ class  SearchBooks extends Component {
       <div>
         <div className="search-books">
           <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <Link className="close-search" to="/">Close</Link>
             <div className="search-books-input-wrapper">
               {/*
                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
