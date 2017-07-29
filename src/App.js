f0ca2b63e11851ff7a4eee80c6ac9e18a6de04a0
Alexander Rosa <alexanderrosa@alexanders-mbp.lan>
2017-07-29 17:59:50 -0600

    feat: Programmatically filter shelves and display correct books using JSX

--- src/App.js
+++ src/App.js
@@ -7,27 +7,26 @@ import './App.css';
 
 class App extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    // showSearchPage: false,
     books: [],
-    bookshelves: ''
   }
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books });
     })
   }
+  updateShelf = (book, shelf) => {
+    console.log(book, shelf);
+    // BooksAPI.update(book, shelf);
+  }
   render() {
     console.log(this.state);
     return (
       <div className='app'>
         <Route exact path='/' render={({ history }) => (
-          <ListBooks />
+          <ListBooks
+            books={this.state.books}
+            onChangeShelf={this.updateShelf}
+          />
         )}/>
       <Route path='/search' render={({ history }) => (
           <SearchBooks />
